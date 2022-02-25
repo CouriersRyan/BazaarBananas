ac8b7a066171a7afc0e8302c54c419216318a15f
Ryan <zhangryan09@gmail.com>
2022-02-24 22:23:33 -0500

    Created empty scripts for planning purposes.

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class Map : MonoBehaviour
+{
+    //TODO Set up Deluanay triangulation to generate a map
+    //TODO Set up A* Alogrithm for path finding.
+    //TODO Set up events to run into.
+    //TODO Set up player with resources.
+    //MVC for player/GUI where it retreaves events to process.
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
