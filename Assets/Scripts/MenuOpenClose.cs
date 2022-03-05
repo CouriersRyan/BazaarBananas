05409bafba43cc418c5045fae7fad67927311dc1
Ryan <zhangryan09@gmail.com>
2022-03-05 16:42:06 -0500

    Classes for handling buttons in menus and GUI.
    MenuField handles each field of converting between resources in the market.
    MenuOpenClose handles buttons which are meant to Open and Close the menus.

---
+++ Assets/Scripts/MenuOpenClose.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Class that handles buttons for opening and closing menus.
+public class MenuOpenClose : MonoBehaviour
+{
+    [SerializeField] private GameObject open;
+    [SerializeField] private GameObject close;
+
+    private void Start()
+    {
+        GameManager.Instance.m_OnEvent.AddListener(ToggleClose);
+        GameManager.Instance.m_OffEvent.AddListener(ToggleClose);
+    }
+
+    // For the buttons, clicking open/close button will run this method.
+    public void ToggleMenu()
+    {
+        GameManager.Instance.ToggleMenu();
+        ToggleOpen();
+        ToggleClose();
+    }
+
+    //Toggles the button that would open the menu.
+    public void ToggleOpen()
+    {
+        open.SetActive(!open.activeInHierarchy);
+    }
+
+    //Toggles the button that would close the menu.
+    public void ToggleClose()
+    {
+        close.SetActive(!close.activeInHierarchy);
+    }
+}
