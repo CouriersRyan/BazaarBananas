0d11ecf16a95791bc3197679462aac838eb35348
Ryan <zhangryan09@gmail.com>
2022-03-06 18:22:20 -0500

    Removed unused assets.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// Stores all the methods used by buttons in the Main Menu
 public class MainMenu : MonoBehaviour
 {
     public void GoToNextScene()
